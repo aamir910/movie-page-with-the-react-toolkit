c5c7facde2be44b07e6d03995b68328887dd63f4
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2024-05-13 15:49:42 +0500

    Update AddTodo.jsx

--- reduxToolkit/src/component/AddTodo.jsx
+++ reduxToolkit/src/component/AddTodo.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {useDispatch} from 'react-redux'
+import { addTodo } from '../App/features/todos/todoSlices'
 const AddTodo = () => {
+    const {Input , setInput} = useState("")
+     const dispatch = useDispatch();
+
+     const addtodohandler =(e) =>{
+         e.preventDefault()
+         dispatch(addTodo( Input) )
+         
+           setInput('')
+     }
   return (
     <div>
-      
+      <form onSubmit={addtodohandler}>
+          
+          <input type="text"
+          placeholder='enter the todo'
+          value={Input}
+          onChange={()=>setInput(e.target.value)} />
+
+      </form>
+      <button>submit</button>
+
     </div>
   )
 }
